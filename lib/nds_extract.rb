6187339a7bf5fd7a52ee2dd6ffce169f6bd0db31
Kellye Greene <kellyecodes@gmail.com>
2019-09-15 19:42:43 -0400

    Adds solution

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,8 +1,27 @@
 require 'directors_database'
-# Call the method directors_database to retrieve the NDS
 
 def directors_totals
   nds = directors_database
-  # Change the code below to pretty print the nds with pp
-  nil
+  index = 0
+  array = []
+
+  while index < nds.length do
+    hash = {}
+    director_index = 0
+    name = nds[index][:name]
+    movie = nds[index][:movies]
+    hash[:name] = name
+    total = 0
+
+    while director_index < movie.length do
+      total = total + movie[director_index][:worldwide_gross]
+      director_index +=1
+    end
+
+    hash[:total] = total
+    array << hash
+
+    index +=1
+  end
+  pp array
 end
